d93a069538d144d9c79709642957fe9e7fcbd8d6
bhagyesh009 <44260187+bhagyesh009@users.noreply.github.com>
2025-04-04 00:56:39 +0530

    Update main.tf

--- windows/main.tf
+++ windows/main.tf
@@ -13,21 +13,28 @@ resource "azurerm_windows_virtual_machine" "vm" {
     disk_size_gb         = var.os_disk_size
   }
 
-  dynamic "data_disk" {
-    for_each = var.data_disks
-    content {
-      name                 = data_disk.value.name
-      disk_size_gb         = data_disk.value.disk_size_gb
-      storage_account_type = data_disk.value.storage_account_type
-      lun                  = data_disk.value.lun
-      caching              = "None"
-    }
-  }
-
   source_image_reference {
     publisher = var.image_reference.publisher
     offer     = var.image_reference.offer
     sku       = var.image_reference.sku
     version   = var.image_reference.version
   }
+}
+
+# Data Disks (Using Managed Disks & Attachments)
+resource "azurerm_managed_disk" "data_disks" {
+  count                = length(var.data_disks)
+  name                 = var.data_disks[count.index].name
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  storage_account_type = var.data_disks[count.index].storage_account_type
+  disk_size_gb         = var.data_disks[count.index].disk_size_gb
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "data_disk_attachment" {
+  count              = length(var.data_disks)
+  managed_disk_id    = azurerm_managed_disk.data_disks[count.index].id
+  virtual_machine_id = azurerm_windows_virtual_machine.vm.id
+  lun               = var.data_disks[count.index].lun
+  caching           = "None"
 }
